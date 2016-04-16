34a7e41a64d02c8487cc8be1c9ab4275a5943d77
Pranav Chendur <pranav@pricpro.in>
2016-04-17 01:57:15 +0530

    Added Location 2

--- slot.php
+++ slot.php
@@ -12,5 +12,10 @@ if ($_GET["slot"]==2) {
 	fwrite($fh, $stringToWrite); // Writes it to the .txt file
 	fclose($fh); 
 }
-
+if ($_GET["slot"]==3) {
+	$fh = fopen("state3.txt", "w") or die("Something went wrong!"); // Opens up the .txt file for writing and replaces any previous content
+	$stringToWrite = $_GET["state"]; // Write either 1 or 0 depending on request from index.html
+	fwrite($fh, $stringToWrite); // Writes it to the .txt file
+	fclose($fh); 
+}
 ?>
