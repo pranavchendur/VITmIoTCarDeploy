b31c801a4381db4452e118c7971da9ac46398702
Pranav Chendur <pranav@pricpro.in>
2016-04-17 05:49:03 +0530

    Azure Table Retry

--- slot.php
+++ slot.php
@@ -19,4 +19,38 @@ if ($_GET["slot"]==3) {
 	fclose($fh); 
 }
 
+//Azure Table Post
+if ($_GET["state"]=="404") $po = "Free"; else $po = "Parked";
+
+/** Using http_build_query() to build parameters */
+$data =array('slot'=>$_GET["slot"],'value'=>$po);
+    
+$url = "http://iotmcare.azure-mobile.net/tables/datastore"; 
+$page = "/tables/datastore"; 
+$headers = array( 
+    "POST ".$page." HTTP/1.0", 
+    "Cache-Control: no-cache", 
+    "X-ZUMO-APPLICATION : DIxyzBoGouxqPaojoTezZFWtiVsdxF24"
+); 
+
+$ch = curl_init(); 
+curl_setopt($ch, CURLOPT_URL,$url); 
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+curl_setopt($ch, CURLOPT_TIMEOUT, 5); 
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers); 
+
+// Apply the XML to our curl call 
+curl_setopt($ch, CURLOPT_POST, 1); 
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data); 
+
+$datac = curl_exec($ch); 
+
+if (curl_errno($ch)) { 
+    print "Error: " . curl_error($ch); 
+} else { 
+    // Show me the result 
+    var_dump($datac); 
+    curl_close($ch); 
+} 
+
 ?>
